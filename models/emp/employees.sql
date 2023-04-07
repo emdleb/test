00e42f30eb75bf585fd64d1a0210775eded50c4d
emdleb@gmail.com <emdleb@gmail.com>
2023-04-07 06:31:01 +0000

    added departures

--- models/emp/employees.sql
+++ models/emp/employees.sql
@@ -20,6 +20,15 @@ salaries as (
 from PC_FIVETRAN_DB.AVSTEP3.SALARIES
 ),
 
+departures as (
+    select
+        emp_no,
+        exit_date,
+        exit_reason
+
+from PC_FIVETRAN_DB.AVSTEP3.DEPARTURES
+),
+
 final as (
     select
         PC_FIVETRAN_DB.AVSTEP3.EMPLOYEES.birth_date,
@@ -29,13 +38,16 @@ final as (
         PC_FIVETRAN_DB.AVSTEP3.EMPLOYEES.last_name,
         PC_FIVETRAN_DB.AVSTEP3.EMPLOYEES.sex,
         PC_FIVETRAN_DB.AVSTEP3.EMPLOYEES.hire_date,
-        PC_FIVETRAN_DB.AVSTEP3.SALARIES.salary
+        PC_FIVETRAN_DB.AVSTEP3.SALARIES.salary,
+        PC_FIVETRAN_DB.AVSTEP3.DEPARTURES.exit_date,
+        PC_FIVETRAN_DB.AVSTEP3.DEPARTURES.exit_reason
 
     FROM PC_FIVETRAN_DB.AVSTEP3.EMPLOYEES
 
     left join PC_FIVETRAN_DB.AVSTEP3.SALARIES using (emp_no)
+    left join PC_FIVETRAN_DB.AVSTEP3.DEPARTURES using (emp_no)
 
-    );
+    )
 
     select * from final
 
